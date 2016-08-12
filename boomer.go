e056fd8c5e2e569ba9062be1c1afdd6942bcc2e6
yzhang3 <yzhang3@linkernetworks.com>
2016-08-12 18:04:05 +0800

    avoid uint overflow; error check

--- boomer.go
+++ boomer.go
@@ -1,33 +1,44 @@
 package boomer
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 // Boomer is a count down timer and can trigger a function.
 type Boomer struct {
 	SECONDS_INIT uint64
 	seconds      uint64
 	f            interface{}
+	armed        bool
+	unarmed      bool
+	boomed       bool
 }
 
 // NewBoomer creates a boomer
-func NewBoomer(seconds uint64, f interface{}) *Boomer {
+func NewBoomer(seconds uint64, f interface{}) (*Boomer, error) {
+	if seconds <= 0 {
+		return nil, errors.New("invalid seconds")
+	}
 	p := &Boomer{}
 	p.SECONDS_INIT = seconds
 	p.seconds = seconds
 	p.f = f
-	return p
+	return p, nil
 }
 
 // Let timer start to count down
 func (p *Boomer) Arm() {
 	go func() {
-		for ; p.seconds >= 0; p.seconds-- {
-			if p.seconds-1 == 0 {
+		for ; p.seconds+1 > 0; p.seconds-- {
+			// fmt.Printf("\r%ds ", p.seconds)
+			if p.seconds == 0 {
 				p.boom(p.f)
 			}
 			time.Sleep(time.Second)
 		}
 	}()
+	p.armed = true
 	return
 }
 
@@ -35,17 +46,28 @@ func (p *Boomer) Arm() {
 func (p *Boomer) Unarm() {
 	p.SECONDS_INIT = 0
 	p.seconds = 0
+	p.unarmed = true
 	return
 }
 
 // Reset time to init
-func (p *Boomer) Rewind() {
+func (p *Boomer) Rewind() error {
+	if !p.armed {
+		return errors.New("cannot rewind, boomer not armed")
+	}
+	if p.unarmed {
+		return errors.New("cannot rewind, boomer unarmed")
+	}
+	if p.unarmed {
+		return errors.New("cannot rewind, boomer has boomed")
+	}
 	p.seconds = p.SECONDS_INIT
-	return
+	return nil
 }
 
 // Trigger function
 func (p *Boomer) boom(f interface{}) {
 	f.(func())()
+	p.boomed = true
 	return
 }
--- example/main.go
+++ example/main.go
@@ -1,22 +1,34 @@
-package example
+package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/zyfdegh/boomer"
 )
 
 func main() {
-	boomer := boomer.NewBoomer(30, f)
+	// uncomment this line in `boomer.go` will print seconds
+	// fmt.Printf("\r%ds ", p.seconds)
+
+	boomer, err := boomer.NewBoomer(5, f)
+	if err != nil {
+		log.Printf("new boomer error: %v", err)
+		return
+	}
 	boomer.Arm()
 
-	time.Sleep(time.Second * 5)
-	boomer.Rewind()
+	time.Sleep(time.Second * 3)
+	err = boomer.Rewind()
+	if err != nil {
+		log.Printf("rewind timer error: %v", err)
+		return
+	}
 
-	boomer.Unarm()
+	//	boomer.Unarm()
 
-	time.Sleep(time.Second * 60)
+	time.Sleep(time.Second * 10)
 }
 
 func f() {
